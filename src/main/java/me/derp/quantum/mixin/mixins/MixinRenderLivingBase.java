43e1035da78ac0c7d6cef69f6763ea2f03e34700
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-10-14 22:33:18 +0200

    Update MixinRenderLivingBase.java

--- src/main/java/me/derp/quantum/mixin/mixins/MixinRenderLivingBase.java
+++ src/main/java/me/derp/quantum/mixin/mixins/MixinRenderLivingBase.java
@@ -3,7 +3,6 @@ package me.derp.quantum.mixin.mixins;
 
 import java.awt.Color;
 import me.derp.quantum.event.events.RenderEntityModelEvent;
-import me.derp.quantum.features.modules.client.Colors;
 import me.derp.quantum.features.modules.render.Chams;
 import me.derp.quantum.features.modules.render.PopChams;
 import me.derp.quantum.util.EntityUtil;
